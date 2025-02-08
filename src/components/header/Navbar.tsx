532ef40a698a2cccb7fd6549b7f8e74a6fa46b8e
aref <arefhukan@gmail.com>
2025-02-08 21:58:59 +0330

    header responsive done

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -5,10 +5,12 @@ import searchIcon from "../../assets/search.svg";
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-evenly w-full h-20">
-      <img src={menuIcon} alt="menu_icon" className="xl:hidden" />
-      <span className="text-black font-extrabold text-3xl">SHOP.CO</span>
-      <div className="flex items-center gap-x-10 curp">
+    <header className="flex items-center justify-between px-5 w-full h-20 md:justify-evenly">
+      <div className="flex gap-x-2 ">
+        <img src={menuIcon} alt="menu_icon" className="md:hidden" />
+        <span className="text-black font-extrabold text-3xl">SHOP.CO</span>
+      </div>
+      <div className="items-center gap-x-10 curp hidden md:flex">
         <span className="text-slate-600 text-lg font-medium">Shop</span>
         <span className="text-slate-600 text-lg font-medium">On Sale</span>
         <span className="text-slate-600 text-lg font-medium">New Arrivals</span>
@@ -16,13 +18,13 @@ const Navbar = () => {
       </div>
       <input
         type="search"
-        className="w-[22%] px-4 py-2 bg-gray-200 outline-none border-none rounded-full "
+        className="w-[22%] px-4 py-2 bg-gray-200 outline-none border-none rounded-full hidden xl:inline-block"
         placeholder="Search for products..."
       />
-      <div className="flex items-center gap-x-10 curp">
+      <div className="flex items-center gap-x-2 curp md:gap-x-10">
         <img src={searchIcon} alt="search_icon" className="xl:hidden" />
         <img src={cartIcon} alt="cartI_icon" />
-        <img src={cartIcon} alt="cartI_icon" />
+        <img src={userIcon} alt="cartI_icon" />
       </div>
     </header>
   );
